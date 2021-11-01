a979c24a5b76c5506657b5456a658c52d3a516c4
joo.d <61643275+joodDev@users.noreply.github.com>
2021-11-01 10:39:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import discord
 import os
+from keep_alive import keep_alive
 from discord.ext import commands
 
-client = discord.Bot()
-
-@client.event
-async def on_ready():
-  print('Online!!')
+client = commands.Bot(command_prefix="m!")
 
 
-@client.slash_command(guild_ids=[902177990916579399])
+@client.slash_command()
 async def ping(ctx):
   await ctx.respond('pong!')
 
+@client.command()
+async def hello(ctx):
+  member = ctx.author
 
+  em = discord.Embed(title = 'Hello! 👋🏻', description = f'Hello {member.mention}!', colour = 0x31bd54)
+
+  await ctx.send(embed = em)
+
+
+keep_alive()
 client.run(os.getenv("token"))
 
 #Watch the tutorial here if you do not understand the code or join my discord server!
